23fee428b70949d8b99d2ebad30cdf1dbf13ca74
prsprshr@gmail.com <123573626+ParasParashar@users.noreply.github.com>
2023-10-02 00:12:24 +0530

    final

--- app/loading.jsx
+++ app/loading.jsx
@@ -4,7 +4,7 @@ import { Loader2 } from "lucide-react"
 
 const loader = () => {
   return (
-    <div className="flex items-center justify-center h-full w-full">
+    <div className="flex items-center justify-center h-screen bgcolorDeatils w-full">
       <Loader2 className="h-32 w-32"/>
     </div>
   )
