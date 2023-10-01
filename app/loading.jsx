24209b6d819b9f300b4ba5f3eb4f59911ac2c2aa
prsprshr@gmail.com <123573626+ParasParashar@users.noreply.github.com>
2023-10-02 00:07:46 +0530

    set loading data

---
+++ app/loading.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Loader2 } from "lucide-react"
+
+const loader = () => {
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      <Loader2 className="h-32 w-32"/>
+    </div>
+  )
+}
+
+export default loader
